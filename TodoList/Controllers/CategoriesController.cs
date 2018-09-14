0f479f88eeebdcec4719a4ccfd8d4297aae871d6
Cris <cristtianopinto@gmail.com>
2018-09-14 15:51:39 +0200

    1 - Création de la Classe CategoriesController avec les methodes: Get - 2 Post Put Delete

--- TodoList/Controllers/CategoriesController.cs
+++ TodoList/Controllers/CategoriesController.cs
@@ -18,6 +18,63 @@ namespace TodoList.Controllers
             return db.Categories.OrderBy(x => x.Nom);
         }
 
+        public IHttpActionResult GetCategorie(int id)
+        {
+            var categorie = db.Categories.Find(id);
+            if(categorie == null)
+            {
+                return NotFound();
+            }
+            return Ok(categorie);
+        }
+
+        public IHttpActionResult PostCategories(Categorie categorie)
+        {
+            if (ModelState.IsValid)//voir le ModelState
+            {
+                db.Categories.Add(categorie);
+                db.SaveChanges();
+
+                return Ok(categorie);
+            }
+            else
+                return BadRequest(ModelState);    
+        }
+
+        public IHttpActionResult PutCategories(int id, Categorie categorie)
+        {
+            if(id != categorie.ID)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (db.Categories.Count(x => x.ID == id) != 1)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(categorie).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        public IHttpActionResult DeleteCategories(int id)
+        {
+            var categorie = db.Categories.Find(id);
+            if (categorie==null)
+            {
+                return NotFound();
+            }
+            db.Entry(categorie).State = System.Data.Entity.EntityState.Deleted;
+            db.SaveChanges();
+
+            return Ok(categorie);
+        }
+
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
